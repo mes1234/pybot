1a0ee73aec99caef1f10535d938a145c5d666489
WK WK <witold.kepczynski@gmail.com>
2021-05-22 12:09:04 +0200

    updt

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -1,6 +1,9 @@
 import debugpy
+print("debugger started")
 debugpy.listen(("localhost", 5678))
+print("listening to external")
 breakpoint()
+print("connected")
 counter = 0
 while True:
     print(f"yello {counter}")
