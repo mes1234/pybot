99e41c29ea1b482c6969ff10dcca2139c3e73840
WK WK <witold.kepczynski@gmail.com>
2021-05-24 21:05:01 +0200

    wip

---
+++ main.py
@@ -0,0 +1,6 @@
+from  driver.driver import start_debugger
+
+print(f"started + {__name__}")
+
+if __name__ == "__main__":
+    start_debugger()
--- src/driver/driver.py
+++ src/driver/driver.py
@@ -1,11 +1,13 @@
 import debugpy
 import time
-print("debugger started")
-debugpy.listen(("0.0.0.0", 5678))
-print("listening to external")
-debugpy.wait_for_client()  # blocks execution until client is attached
-print("connected")
-counter = 0
-while True:
-    time.sleep(1)
-    print(f"yello {counter}")
+
+def start_debugger():
+    print("debugger started")
+    debugpy.listen(("0.0.0.0", 5678))
+    print("listening to external")
+    debugpy.wait_for_client()  # blocks execution until client is attached
+    print("connected")
+    counter = 0
+    while True:
+        time.sleep(1)
+        print(f"yello {counter}")
