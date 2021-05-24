1316292ef3dbf5452ada8c9a3c5e2bc4c6a8760b
WK WK <witold.kepczynski@gmail.com>
2021-05-25 00:36:42 +0200

    ok

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ from  eventbus.loop import EventBus
 def handler(msg):
     print(f"this is {msg}")
 
+async def blabl():
+    while(True):
+        print("blalbl")
+        await asyncio.sleep(1)
+
 async def run(topic:str):
     eb = EventBus("raspberrypi.local:4222")
     await eb.connect()
@@ -20,7 +25,8 @@ if __name__ == "__main__":
     
     loop = asyncio.get_event_loop()
     tasks = []
-    tasks.append(asyncio.s(run("DEM1")))
+    tasks.append(asyncio.ensure_future(run("DEM1")))
+    tasks.append(asyncio.ensure_future(blabl()))
     # tasks.append(asyncio.ensure_future(run("DEM2")))
     # tasks.append(asyncio.ensure_future(run("DEM3")))
     loop.run_until_complete(asyncio.wait(tasks))
